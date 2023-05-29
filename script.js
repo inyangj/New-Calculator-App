165a724c58980989173333982a5fbab6d6108a4c
inyangj <inyangjohnson01@gmail.com>
2023-05-29 19:45:56 +0100

    News Calculator App

---
+++ script.js
@@ -0,0 +1,59 @@
+const output = document.querySelector('.output');
+const buttons = document.querySelectorAll('.button');
+const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
+const  body = document.querySelector('.container');
+const toggle = document.getElementById('toggleDark');
+
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('fa-moon');
+    if(this.classList.toggle('fa-sun')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.boxShadow = 'none'
+        output.style.color = 'black'
+        equalButton.style.background = 'rgb(9, 52, 30)'
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.boxShadow = '5px 5px 10px #b6acac, -5px -5px 10px #ffff';
+        output.style.color = 'white'
+        equalButton.style.background = 'rgb(16, 215, 112)'
+        body.style.transition = '2s';
+    }
+});
+
+
+buttons.forEach(button => {
+button.addEventListener('click', () => {
+    const buttonValue = button.innerText;
+    if (buttonValue === 'AC') {
+    output.textContent = '0';
+    } else if (buttonValue === '%') {
+        output.textContent += ' * .01'
+    } else if (buttonValue === '÷') {
+        output.textContent += '/'
+    } else if (buttonValue === '×') {
+        output.textContent += '*'
+    }
+    else if (buttonValue === '=') {
+    try {
+        output.textContent = eval(output.textContent);
+    } catch (error) {
+        output.textContent = 'Error';
+    }
+    } else {
+    if (output.textContent === '0') {
+        output.textContent = buttonValue;
+    } else {
+        output.textContent += buttonValue;
+    }
+    }
+});
+});
+
+clearButton.addEventListener('click', () => {
+    output.textContent = output.textContent.slice (0, -1)
+});
